655443bb76d0014ea99187e47be151e0a24be434
kiy0taka <kiy0taka333@gmail.com>
2023-02-12 20:06:26 +0900

    fix delete dtb_block

--- zap/delete_data.sh
+++ zap/delete_data.sh
@@ -13,7 +13,7 @@ delete from dtb_payment_option where delivery_id in (select id from dtb_delivery
 delete from dtb_delivery where create_date between now() - interval '24:00' and now() - interval '00:00:06';
 delete from dtb_payment where create_date between now() - interval '24:00' and now() - interval '00:00:06';
 delete from dtb_block_position where layout_id in (select id from dtb_layout where create_date between now() - interval '24:00' and now() - interval '00:00:05');
-delete from dtb_block where create_date between now() - interval '24:00' and now() - interval '00:00:06';
+delete from dtb_block where create_date between now() - interval '24:00' and now() - interval '00:00:06' and id not in (select distinct block_id from dtb_block_position);
 delete from dtb_layout where create_date between now() - interval '24:00' and now() - interval '00:00:06';
 delete from dtb_category where create_date between now() - interval '24:00' and now() - interval '00:00:06';
 delete from dtb_class_category where class_name_id in (select id from dtb_class_name where create_date between now() - interval '24:00' and now() - interval '00:00:05');
