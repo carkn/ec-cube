74469148321fc17a7485b782bc454145d86b45d3
carkn <carbuncle.panic@gmail.com>
2023-06-13 10:39:07 +0900

    add add_CancelDeletionEventSubscriber

--- zap/add_CancelDeletionEventSubscriber.sh
+++ zap/add_CancelDeletionEventSubscriber.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-php bin/template_jp.php
-#rm -rf app/Plugin/*
 echo '<?php
 namespace Eccube\Doctrine\EventSubscriber;
 use Doctrine\Common\EventSubscriber;
@@ -19,8 +17,3 @@ class CancelDeletionEventSubscriber implements EventSubscriber
   }
 }' > CancelDeletionEventSubscriber.php
 sed -i.bak -e 's_$fs->remove_// $fs->remove_' src/Eccube/Controller/Admin/Content/PageController.php
-rm -f app/config/eccube/packages/dev/web_profiler.yaml
-bin/console doctrine:database:create --env=dev
-bin/console doctrine:schema:create --env=dev
-bin/console eccube:fixtures:load --env=dev
-chmod -R 777 html
